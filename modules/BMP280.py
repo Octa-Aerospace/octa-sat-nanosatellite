2a3a9dbb7c018c4a67b801e6540ff98445472653
CxrlosKenobi <yolitopinto@gmail.com>
2021-10-04 00:20:06 -0300

    Update On-Board Computer source code

---
+++ modules/BMP280.py
@@ -0,0 +1,58 @@
+from subprocess import PIPE, Popen
+from bmp280 import BMP280
+
+try:
+    from smbus2 import SMBus
+
+except ImportError:
+    from smbus import SMBus
+
+
+class BMP:
+    def __init__(self):
+        # Initialize the BMP280
+        self.bus = SMBus(1)
+        self.bmp280 = BMP280(i2c_dev=self.bus)
+
+    def temp(self, decimal):
+        temperature = round(self.bmp280.get_temperature(), decimal)
+        return temperature
+
+    def press(self, decimal):
+        pressure = round(self.bmp280.get_pressure(), decimal)
+        return pressure
+
+    def alt(self, decimal):
+        baseline_values = []
+        baseline_size = 100
+
+        # Calibration with 100 sampling
+        for i in range(baseline_size):
+            baseline_values.append(round(self.bmp280.get_pressure(), 4))
+
+        baseline = sum(baseline_values[:-25]) / len(baseline_values[:-25])
+        altitude = round(self.bmp280.get_altitude(qnh=baseline), decimal)
+        return altitude
+
+    # # Gets the CPU temperature in degrees C
+    # def get_cpu_temperature(self):
+    #     process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
+    #     output, _error = process.communicate()
+    #     #return float(output[output.index('=') + 1:output.rindex("'")])
+    #     return output
+
+    # def comps_temp(self):
+    #     factor = 1.2  # Smaller numbers adjust temp down, vice versa
+    #     smooth_size = 10  # Dampens jitter due to rapid CPU temp changes
+    #     cpu_temps = []
+    #     cpu_temp = self.get_cpu_temperature()
+    #     cpu_temps.append(cpu_temp)
+
+    #     if len(cpu_temps) > smooth_size:
+    #         cpu_temps = cpu_temps[1:]
+
+    #     smoothed_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+    #     raw_temp = self.bmp280.get_temperature()
+    #     comp_temp = raw_temp - ((smoothed_cpu_temp - raw_temp) / factor)
+    #     # print("Compensated temperature: {:05.2f} *C".format(comp_temp))
+    #     return comp_temp
---
+++ modules/HDC1080.py
@@ -0,0 +1,16 @@
+from modules import SDL_Pi_HDC1080
+import sys
+import os
+
+class HDC:
+	def __init__(self):
+		sys.path.append('./modules/SDL_Pi_HDC1080_Python3')
+		self.hdc1080 = SDL_Pi_HDC1080.SDL_Pi_HDC1080()
+
+	def temp(self, decimal):
+		temperature = round(self.hdc1080.readTemperature(), decimal)
+		return temperature
+
+	def hum(self, decimal):
+		humidity = round(self.hdc1080.readHumidity(), decimal)
+		return humidity
