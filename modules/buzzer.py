3901887bd2d9d2c3598972a330249433021ee66a
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-11-14 16:12:14 -0300

    Fix: some pin definition error

--- modules/buzzer.py
+++ modules/buzzer.py
@@ -2,26 +2,25 @@ import RPi.GPIO as GPIO
 from time import *
 
 class Buzzer:
-	def __init__(self):
-		self.pin = 12
+	def __init__(self, pin):
+		self.pin = pin
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(self.pin, GPIO.OUT)
 		self.p = GPIO.PWM(self.pin, 300)
 
-	def beep(self, pin):
+	def beep(self):
 		GPIO.setwarnings(False)
-		GPIO.output(pin, True)
+		GPIO.output(self.pin, True)
 		self.p.start(0)
 		self.p.ChangeDutyCycle(100)
 		self.p.ChangeFrequency(100)
 		sleep(0.5)
-		p.stop()
+		self.p.stop()
 
-		GPIO.output(pin, False)
+		GPIO.output(self.pin, False)
 		sleep(2)
 
 		GPIO.cleanup()
 
-Buzzer = Buzzer()
-Buzzer.beep(12)
-
+Buzzer = Buzzer(12)
+Buzzer.beep()
